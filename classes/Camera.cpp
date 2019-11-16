c77512e31873f199f171b031b33f646ed28825fa
Bruno Ferreira Coelho <bfc1@outlook.com>
2019-11-16 18:51:18 -0300

    camera

--- classes/Camera.cpp
+++ classes/Camera.cpp
@@ -7,58 +7,78 @@
 
 #include "Camera.h"
 
-Camera::Camera(){
+Camera::Camera()
+{
 }
 
-void Camera::create(){
+void Camera::create()
+{
     view = {0.0, 6.0, 10.0};
     look = {0.0, 0.0, 0.0};
     view_up = {0.0, 1.0, 0.0};
     inside = false;
 }
 
-void Camera::update(Submarine &sub){
-    if (!inside){
-        view = {(GLfloat)sub.getX(), (GLfloat)sub.getY() + 6 , (GLfloat)sub.getZ() + 10 };
+void Camera::update(Submarine &sub)
+{
+    if (!inside)
+    {
+        view = {(GLfloat)sub.getX(), (GLfloat)sub.getY() + 6, (GLfloat)sub.getZ() + 10};
         look = {(GLfloat)sub.getX(), (GLfloat)sub.getY(), (GLfloat)sub.getZ()};
-    }else{
+    }
+    else
+    {
         Matrix dir = sub.getDirector();
-        view = {(GLfloat)sub.getX(), (GLfloat)sub.getY(), (GLfloat)sub.getZ()};
-        look = {(GLfloat)dir[0][0] + 5,
-                (GLfloat)dir[1][0],
-                (GLfloat)dir[2][0] + 5};
+
+        view = {
+            (GLfloat)(sub.getX() + 5 * dir[0][0]),
+            (GLfloat)(sub.getY() + 5 * dir[1][0]),
+            (GLfloat)(sub.getZ() + 5 * dir[2][0])};
+
+        look = {
+            (GLfloat)(sub.getX() + 15 * dir[0][0]),
+            (GLfloat)(sub.getY() + 15 * dir[1][0]),
+            (GLfloat)(sub.getZ() + 15 * dir[2][0])};
     }
 }
 
-void Camera::change_view(){
+void Camera::change_view()
+{
     inside = !inside;
 }
 
-const bool Camera::getInside(){
+const bool Camera::getInside()
+{
     return inside;
 }
 
-const GLfloat Camera::getX(){
+const GLfloat Camera::getX()
+{
     return view[0];
 }
 
-const GLfloat Camera::getY(){
+const GLfloat Camera::getY()
+{
     return view[1];
 }
 
-const GLfloat Camera::getZ(){
+const GLfloat Camera::getZ()
+{
     return view[2];
 }
 
-const GLfloat Camera::lookX(){
+const GLfloat Camera::lookX()
+{
     return look[0];
 }
 
-const GLfloat Camera::lookY(){
+const GLfloat Camera::lookY()
+{
     return look[1];
 }
 
-const GLfloat Camera::lookZ(){
+const GLfloat Camera::lookZ()
+{
     return look[2];
 }
 
