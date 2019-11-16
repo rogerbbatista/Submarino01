2307578ce808da42d451c5f03ea5dbbe6c3b51df
Bruno Ferreira Coelho <bfc1@outlook.com>
2019-11-16 12:06:47 -0300

    organizando classes

---
+++ classes/Object.h
@@ -0,0 +1,51 @@
+#ifndef OBJECT_H
+#define OBJECT_H
+
+#include <GL/gl.h>
+#include <GL/glut.h>
+#include <bits/stdc++.h>
+
+struct Vertex
+{
+    GLfloat x;
+    GLfloat y;
+    GLfloat z;
+};
+
+struct Texture
+{
+    GLfloat t1;
+    GLfloat t2;
+};
+
+struct Face
+{
+    int v1;
+    int v2;
+    int v3;
+    int t1;
+    int t2;
+    int t3;
+    int n1;
+    int n2;
+    int n3;
+    
+    void reindex();
+};
+
+class Object
+{
+private:
+    std::vector<Vertex> vertices;
+    std::vector<Texture> textures;
+    std::vector<Vertex> normals;
+    std::vector<Face> faces;
+
+public:
+    Object() {}
+    Object(std::string name);
+    void draw();
+    void charge(std::string name);
+};
+
+#endif // OBJECT_H
---
+++ classes/Submarine.h
@@ -0,0 +1,11 @@
+#ifndef SUBMARINE_H
+#define SUBMARINE_H
+
+#include "Object.h"
+
+class Submarine : Object
+{
+    
+};
+
+#endif // SUBMARINE_H
