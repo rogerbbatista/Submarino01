42e10fb97c8dd88c26d5e92b4955e0c6425b3569
Bruno Ferreira Coelho <bfc1@outlook.com>
2019-11-16 10:56:34 -0300

    test update

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 
-gcc -ggdb3 -O0 -o test -std=c99 -Wall -Wextra -pedantic test.c -lGL -lGLU -lglut
-./test
+# gcc -ggdb3 -O0 -o test -std=c99 -Wall -Wextra -pedantic test.c -lGL -lGLU -lglut
+g++ test.cpp -lglut -lGLU -lGL -o test.out
+./test.out $1
