1da12e9acc0bf2ade52f33f94dbe5fe60cef80ce
rogerbbatista <rogerbbatista@hotmail.com>
2019-12-01 21:22:50 -0300

    mds isso tem q acabar

--- classes/Ocean.cpp
+++ classes/Ocean.cpp
@@ -13,14 +13,14 @@ Ocean::Ocean()
 
 void Ocean::draw()
 {
-    draw_water();
-    glColor4f(color_water[0], color_water[1], color_water[2], color_water[3]);
+    glColor4f(0.0, 0.01, 1.0, 1);
     draw_face(4, 7, 3, 0);
     draw_face(5, 6, 2, 1);
     draw_face(4, 5, 1, 0);
     draw_face(7, 6, 2, 3);
     glColor4f(color_ground[0], color_ground[1], color_ground[2], color_ground[3]);
     draw_face(4, 5, 6, 7);
+    draw_water();
 }
 
 void Ocean::draw_face(int a, int b, int c, int d)
@@ -35,22 +35,26 @@ void Ocean::draw_face(int a, int b, int c, int d)
 
 void Ocean::draw_water()
 {
-    glDisable(GL_LIGHTING);
-    water[0].setPosition(water[0].getX(), water[0].getY(), water[0].getZ() - 1);
-    water[1].setPosition(water[1].getX(), water[1].getY(), water[1].getZ() - 1);
-    if (water[second].getZ() == 0){
-        second = second ^ 1;
-        water[second].setPosition(water[second].getX(), water[second].getY(), 400);
-    }
-    water[0].draw();
-    water[1].draw();
-    glEnable(GL_LIGHTING);
+    glPushMatrix();
+
+        glDisable(GL_LIGHTING);
+        water[0].setPosition(water[0].getX(), water[0].getY(), water[0].getZ() - 1);
+        water[1].setPosition(water[1].getX(), water[1].getY(), water[1].getZ() - 1);
+        if (water[second].getZ() == 0){
+            second = second ^ 1;
+            water[second].setPosition(water[second].getX(), water[second].getY(), 400);
+        }
+        water[0].draw();
+        water[1].draw();
+        glEnable(GL_LIGHTING);
+
+    glPopMatrix();
 }
 
 void Ocean::create(double tamanho)
 {
     GLfloat tam = (GLfloat)tamanho;
-    color_water = {0.0, 0.0, 1.0, 0.8};
+    color_water = {0.0, 0.0, 1.0, 1.0};
     color_ground = {0.5, 0.5, 0.5, 1.0};
     vertices = {
         {-tam, 0, tam},
